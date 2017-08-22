e98d4db1230331ddf79a22ab7885a5dc72bcc29b
tammy le <tammyle@tammys-MBP.home>
2017-08-21 23:57:36 -0700

    getMovie function added

--- liri.js
+++ liri.js
@@ -37,3 +37,56 @@ function getTwitter() {
         }
     });
 };
+
+//omdb movie function 
+function getMovie() {
+
+    var request = require('request');
+    var movieArr = process.argv;
+    var movieName = '';
+
+    //if user dont type in any movie name, the program will output data for the movie 'Mr. Nobody.'
+    if (process.argv[3] === undefined) {
+        movieName = 'Mr. Nobody'
+    }
+
+    // if user type in some moview name
+    else {
+        for (var i = 3; i < movieArr.length; i++) {
+            //if the movie has more than 1 words
+            if (i > 3 && i < movieArr.length) {
+                movieName = movieName + ' ' + movieArr[i]
+            }
+            //if the movie contains only one word
+            else {
+                movieName += movieArr[i];
+            }
+        }
+
+    }
+
+
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    //console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            //console.log(JSON.parse(body));
+            console.log('\nTitle: ' + JSON.parse(body).Title);
+            console.log('Release Year: ' + JSON.parse(body).Year);
+            console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
+            if (JSON.parse(body).Ratings[1] === undefined) {
+                console.log('There is no Rotten Tomatoes Rating for this movie!')
+            }
+            else {
+                console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value);
+            }
+            console.log('Country: ' + JSON.parse(body).Country);
+            console.log('Language: ' + JSON.parse(body).Language);
+            console.log('Plot: ' + JSON.parse(body).Plot);
+            console.log('Actors: ' + JSON.parse(body).Actors + '\n');
+        }
+    });
+};
+
